d62f21f3988392b51c0abc6a1cf33de1049628a3
LAPTOP-FILIP\admin <f.regenczuk@outlook.com>
2018-04-04 10:45:08 +0200

    Edit
    Edycja enterów, żeby to jakoś lepiej wyglądało. No i skasowanie jakiegoś oczywistego komentarza "// public metods" zaraz po "public:"

--- button.h
+++ button.h
@@ -7,18 +7,19 @@
 class Button:public QObject, public QGraphicsRectItem
 {
      Q_OBJECT
+
 public:
 
     Button(QString name, QGraphicsItem * parent = NULL);
-
-    //public methods
 
     //events
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
     void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
     void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
+
 signals:
     void clicked();
+
 private:
     QGraphicsTextItem *text;
 };
